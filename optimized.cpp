2153d5cf2d102c204df9cb0e5eca1ceb40578b15
adidavid014 <adidavid014@gmail.com>
2024-02-26 19:24:08 -0500

    added logic for insertion into trie

--- optimized.cpp
+++ optimized.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 #include <cstring>
 
-#define MAX_NODES 10000
+#define NUM_NODES 10000
 #define NUM_CHARS 95
+
+using namespace std;
 
 struct Node{
     int children[NUM_CHARS];
@@ -18,4 +20,32 @@ struct Node{
         end = false;
     }    
 };
+
+vector<Node> trie(NUM_NODES);
+int nextIndex = 1;
+
+void insert(char* key, size_t len, int val){
+    int currIndex = 0; //root
+
+    for(size_t i = 0; i < len; i++){
+        int charIndex = key[i] - ' '; //ASCII range is 0x20 to 0x7E
+
+        if(charIndex < 0 || charIndex > 95){
+            //error
+            continue;
+        }
+
+        if(trie[currIndex].children[charIndex] == -1){ //no child inserted in this index yet
+            trie[currIndex].children[charIndex] = trie.size();
+            trie.push_back(Node());
+        }
+
+        currIndex = trie[currIndex].children[charIndex];
+    }
+    trie[currIndex].end = true;
+    
+    if(val > trie[currIndex].val){
+        trie[currIndex].val = val;
+    }
+}
 
